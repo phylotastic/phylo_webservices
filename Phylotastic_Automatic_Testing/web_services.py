8d9bd4697c0ece3c5a2d72fbc0d8df1ef29cd9d5
thanhnh-infinity <thanhnh2911@gmail.com>
2016-04-11 21:41:14 -0600

    Update WS 1, Ws2 , WS3

--- Phylotastic_Automatic_Testing/web_services.py
+++ Phylotastic_Automatic_Testing/web_services.py
@@ -10,6 +10,9 @@ WS_1_HEADER = {'content-type':'application/json'};
 
 WS_2_RESOURCES_URI = "http://phylo.cs.nmsu.edu:5004/phylotastic_ws/fn/names_text";
 WS_2_HEADER = {'content-type':'application/json'};
+
+WS_3_RESOURCES_URL_GET = "http://phylo.cs.nmsu.edu:5004/phylotastic_ws/tnrs/ot/resolve"
+WS_3_HEADER = {'content-type':'application/json'}
 #-------------------------------------------------------------------------------
 #Each function is testing tool for each Web Service in document
 #https://github.com/phylotastic/phylo_services_docs/blob/master/ServiceDescription/PhyloServicesDescription.md
@@ -27,8 +30,6 @@ def testService_FindScientificNamesOnWebPages_WS_1(param_url,expected_output):
     response = requests.get(WS_1_RESOURCES_URI, params=encoded_param_structure, headers=WS_1_HEADER)
     if (response.status_code == requests.codes.ok):
        ws1_json_result = response.text
-       #print "Web Service 1 : FindScientificNames Result : %s " %(str(ws1_json_result))
-       #Check output data of web service is correct and consistent : JSON format, code == 200,existed JSON object scientificNames
        if (isJSON(str(ws1_json_result)) == False):
            print("Error : Web Service 1's result is not JSON Format")
            exit(1)
@@ -55,11 +56,9 @@ def testService_FindScientificNamesOnText_WS_2(param_text,expected_output):
     	'text' : param_text
     }
     encoded_param_structure = urllib.urlencode(param_structure)
-    response = requests.get(WS_2_RESOURCES_URI, params=encoded_param_structure, headers=WS_1_HEADER)
+    response = requests.get(WS_2_RESOURCES_URI, params=encoded_param_structure, headers=WS_2_HEADER)
     if (response.status_code == requests.codes.ok):
        ws2_json_result = response.text
-       #print "Web Service 1 : FindScientificNames Result : %s " %(str(ws1_json_result))
-       #Check output data of web service is correct and consistent : JSON format, code == 200,existed JSON object scientificNames
        if (isJSON(str(ws2_json_result)) == False):
            print("Error : Web Service 2's result is not JSON Format")
            exit(1)
@@ -78,6 +77,35 @@ def testService_FindScientificNamesOnText_WS_2(param_text,expected_output):
        print("Pass : Returned data contains expected output")
        return True
     else:
-       print("Error : Exit 1")
+       print("Error : Exit 2")
+       return False
+       exit(1)
+def testService_ResolveScientificNamesOpenTreeWS_WS_3_GET(param_names,expected_output):
+    param_structure = {
+    	'names' : param_names
+    }
+    encoded_param_structure = urllib.urlencode(param_structure)
+    response = requests.get(WS_3_RESOURCES_URL_GET, params=encoded_param_structure, headers=WS_3_HEADER)
+    if (response.status_code == requests.codes.ok):
+       ws3_json_result = response.text
+       if (isJSON(str(ws3_json_result)) == False):
+           print("Error : Web Service 3's result is not JSON Format")
+           exit(1)
+       print("Pass : Returned data is JSON format")
+       json_object = json.loads(str(ws3_json_result))
+       if (type(json_object["resolvedNames"]) is not list):
+           print("Error : JSON format is not correct")
+           exit(1)
+       print("Pass : Returned data contains object 'resolvedNames'")
+       #Check correct output data
+       set_expected_ouput = set(expected_output)
+       set_result = set(json_object["resolvedNames"][0]["synonyms"])
+       if (not set_expected_ouput.issubset(set_result)):
+           print("Error : Web Service's result could be in-correct");
+           exit(1)
+       print("Pass : Returned data contains expected output")
+       return True
+    else:
+       print("Error : Exit 3")
        return False
        exit(1)
