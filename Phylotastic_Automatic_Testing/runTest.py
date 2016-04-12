c65e029d6f9f9fd594170ac45b2173057fda14a1
thanhnh-infinity <thanhnh2911@gmail.com>
2016-04-11 18:06:45 -0600

    Update WS 1

--- Phylotastic_Automatic_Testing/runTest.py
+++ Phylotastic_Automatic_Testing/runTest.py
@@ -1,18 +1,29 @@
-def testWebService():
-   import urllib2
-   wsResult = urllib2.urlopen("http://127.0.0.1:8080/index").read()
-   if (wsResult == "Hello World!"):
-       print "Pass"
-       exit(0)
-   if (wsResult == "It fails"):
-       print "Fail"
-       exit(1)
-def testNormal(flag):
-   if (flag == "PASS"):
-       print "PASS"
-       exit(0)
-   if (flag == "FAIL"):
-       print "FAIL"
-       exit(1)
-#testWebService();
-testNormal("PASS")
+import web_services
+
+########################################################
+#Test Web Service 1 : Find Scientific Names on web pages
+#Document : https://github.com/phylotastic/phylo_services_docs/blob/master/ServiceDescription/PhyloServicesDescription.md
+########################################################
+print "========================================================="
+url="https://en.wikipedia.org/wiki/Plain_pigeon"
+print "Start Test WS 1 : Find Scientific Names on web pages"
+print "Case 1 : Paramter URL = %s \n" %(str(url))
+result_case_1 = web_services.testService_FindScientificNamesOnWebPages_WS_1(url)
+print "---------------------------------------------------------"
+url="http://en.wikipedia.org/wiki/Ant"
+print "Start Test WS 1 : Find Scientific Names on web pages"
+print "Case 2 : Paramter URL = %s \n" %(str(url))
+result_case_2 = web_services.testService_FindScientificNamesOnWebPages_WS_1(url)
+print "---------------------------------------------------------"
+url="http://www.fws.gov/westvirginiafieldoffice/PDF/beechridgehcp/Appendix_D_Table_D-1.pdf"
+print "Start Test WS 1 : Find Scientific Names on web pages"
+print "Case 3 : Paramter URL = %s \n" %(str(url))
+#result_case_3 = web_services.testService_FindScientificNamesOnWebPages_WS_1(url)
+result_case_3 = True
+print "========================================================="
+
+
+if (result_case_1 == True and result_case_2 == True and result_case_3 == True):
+    exit(0)
+else:
+    exit(1)
--- Phylotastic_Automatic_Testing/web_services.py
+++ Phylotastic_Automatic_Testing/web_services.py
@@ -1,11 +1,40 @@
-import cherrypy
-  
-class HelloWorld(object):
-    def index(self):
-        return "Hello World!"
-    def testFail(self):
-        return "It fails"
-    index.exposed = True
-    testFail.exposed = True
-
-cherrypy.quickstart(HelloWorld())
+import urllib2
+import urllib
+import datetime
+import json
+import requests
+#Define Resources URI for Testing Web Service
+#https://github.com/phylotastic/phylo_services_docs/blob/master/ServiceDescription/PhyloServicesDescription.md
+WS_1_RESOURCES_URI = "http://phylo.cs.nmsu.edu:5004/phylotastic_ws/fn/names_url";
+WS_1_HEADER = {'content-type':'application/json'};
+#-------------------------------------------------------------------------------
+#Each function is testing tool for each Web Service in document
+#https://github.com/phylotastic/phylo_services_docs/blob/master/ServiceDescription/PhyloServicesDescription.md
+def isJSON(string):
+    try:
+       json_object = json.loads(string)
+    except ValueError, e:
+       return False
+    return True
+def testService_FindScientificNamesOnWebPages_WS_1(param_url):
+    param_structure = {
+    	'url' : param_url
+    }
+    encoded_param_structure = urllib.urlencode(param_structure)
+    response = requests.get(WS_1_RESOURCES_URI, params=encoded_param_structure, headers=WS_1_HEADER)
+    if (response.status_code == requests.codes.ok):
+       ws1_json_result = response.text
+       #print "Web Service 1 : FindScientificNames Result : %s " %(str(ws1_json_result))
+       #Check output data of web service is correct and consistent : JSON format, code == 200,existed JSON object scientificNames
+       if (isJSON(str(ws1_json_result)) == False):
+           print("Error ; Web Service 1's result is not JSON Format")
+           exit(1)
+       json_object = json.loads(str(ws1_json_result))
+       if (type(json_object["scientificNames"]) is not list):
+           print("Error ; JSON format is not correct")
+           exit(1)
+       return True
+    else:
+       print("Error : Exit 1")
+       exit(1)
+    return ws1_json_result
