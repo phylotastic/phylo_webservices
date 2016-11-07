58ab4488169df7afa78f29f419a7846b88277e97
Abu Saleh <abusalehmdtayeen@gmail.com>
2016-11-06 21:05:08 -0700

    TreeViewer service version 2 deployed

---
+++ TreeViewer_dev2/ete3_helper/tree_handler.py
@@ -0,0 +1,193 @@
+import time
+import string
+import random
+import re
+import os
+import logging as log
+from ete3 import Tree, TreeStyle
+from ete3.parser.newick import NewickError
+
+def timeit(f):
+    def a_wrapper_accepting_arguments(*args, **kargs):
+        t1 = time.time()
+        r = f(*args, **kargs)
+        print " %0.3f secs: %s" %(time.time() - t1, f.__name__)
+        return r
+    return a_wrapper_accepting_arguments
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+class WebTreeHandler(object):
+    def __init__(self, newick_str, tid, actions=None, style=None):
+        self.tree = None
+        self.treenewick = newick_str
+
+        self.treeid = tid
+        self.mapid = "map_" + tid
+        self.imgid = "img_" + tid
+        self.boxid = 'box_' + tid
+
+        self.treeconfig_obj = None
+
+    def get_node_descendants(self, node_id):
+         target = self.tree.search_nodes(_nid=int(node_id))[0]
+         des_nodes = target.get_descendants('preorder')
+         return des_nodes
+
+    def get_tree_leaves(self):
+         return [leaf for leaf in self.tree.iter_leaves()]
+    
+    def init_nodeids(self):
+        # Initialze node internal IDs
+        for index, n in enumerate(self.tree.traverse('preorder')):
+            n._nid = index
+
+    def parse_newick(self):
+        try:
+           self.tree = Tree(self.treenewick)
+        except NewickError:
+           try:
+              self.tree = Tree(self.treenewick, format=1)
+           except NewickError as e:
+              return "Newick Parsing Error: "+str(e)
+
+        self.init_nodeids()
+        return True
+
+    def set_actions(self, actions=None):
+        if actions is None:
+           self.tree.actions = NodeActions()
+        else:
+           self.tree.actions = actions
+
+    def set_style(self, style=None):
+        if style is None:
+           self.tree.tree_style = TreeStyle()
+        else:
+           self.tree.tree_style = style
+
+    def set_tree_config(self, tcofg):
+        self.treeconfig_obj = tcofg
+
+    @timeit
+    def redraw(self, top_offset=0,left_offset=0):
+        print "in redraw topoffset:"+str(top_offset)+" leftoffset:"+str(left_offset)
+        #print "Inside redraw calling tree.render()"
+        #os.environ["DISPLAY"]=":0" # Used by ete to render images
+        base64_img, img_map = self.tree.render("%%return.PNG", tree_style=self.tree.tree_style)
+        #print "Inside redraw calling get_html_map()...."
+        html_map = self.get_html_map(img_map)
+
+        ete_link = '<div style="margin:0px;padding:0px;text-align:left;"><a href="http://etetoolkit.org" style="font-size:10pt;" target="_blank" >Powered by etetoolkit</a></div>'
+        #html_img = """<img id="%s" class="ete_tree_img" USEMAP="#%s" onLoad="javascript:bind_popup(%s,%s);" src="data:image/gif;base64,%s">""" %(self.imgid, self.mapid, left_offset, top_offset, base64_img)
+        html_img = """<img id="%s" class="ete_tree_img" USEMAP="#%s" onLoad="javascript:bind_popup(%s,%s);" src="data:image/gif;base64,%s">""" %(self.imgid, self.mapid, left_offset, top_offset, base64_img)
+
+        tree_div_id = self.boxid
+        #print "returning html from redraw method...."
+        return html_map+ '<div id="%s" >'%tree_div_id + html_img + ete_link + "</div>"
+
+    #------------------------------------------
+    def save_image(self, img_format):
+        img_url = os.path.join("http://phylo.cs.nmsu.edu:8080/TreeViewer/demo/tmp_dev/", self.treeid+"."+img_format)
+        img_path = os.path.join("/var/www/html/TreeViewer/demo/tmp_dev/", self.treeid+"."+img_format)        
+        img = self.tree.render(img_path, tree_style=self.tree.tree_style)
+        #print "returning from save image"
+        return '<a target="_blank" href="%s">Download Image</a>' %(img_url)
+    #------------------------------------------
+    def get_html_map(self, img_map):
+        html_map = '<MAP NAME="%s" class="ete_tree_img">' %(self.mapid)
+        #print "get_html_map method called......."
+        if img_map["nodes"]:
+            for x1, y1, x2, y2, nodeid, text in img_map["nodes"]:
+                text = "" if not text else text
+                area = img_map["node_areas"].get(int(nodeid), [0,0,0,0])
+                html_map += """ <AREA SHAPE="rect" COORDS="%s,%s,%s,%s"
+                                onMouseOut='unhighlight_node();'
+                                onMouseOver='highlight_node("%s", "%s", "%s", %s, %s, %s, %s);'
+                                onClick='show_actions("%s", "%s");'
+                                href="javascript:void('%s');">""" %\
+                    (int(x1), int(y1), int(x2), int(y2),
+                     self.treeid, nodeid, text, area[0], area[1], area[2]-area[0], area[3]-area[1],
+                     self.treeid, nodeid,
+                     nodeid)
+
+        if img_map["faces"]:
+            for x1, y1, x2, y2, nodeid, text in img_map["faces"]:
+                text = "" if not text else text
+                area = img_map["node_areas"].get(int(nodeid), [0,0,0,0])
+                html_map += """ <AREA SHAPE="rect" COORDS="%s,%s,%s,%s"
+                                onMouseOut='unhighlight_node();'
+                                onMouseOver='highlight_node("%s", "%s", "%s", %s, %s, %s, %s);'
+                                onClick='show_actions("%s", "%s", "%s");'
+                                href='javascript:void("%s");'>""" %\
+                    (int(x1),int(y1),int(x2),int(y2),
+                     self.treeid, nodeid, text, area[0], area[1], area[2]-area[0], area[3]-area[1],
+                     self.treeid, nodeid, text,
+                     text,
+                     )
+        html_map += '</MAP>'
+        #print "returning html from get_html_map()...."
+        return html_map
+    
+    #---------------------------------------
+    def get_tree_node(self, nodeid):
+        target_node = self.tree.search_nodes(_nid=int(nodeid))[0]
+        return target_node
+
+    def get_node_name(self, nodeid):
+        target_node = self.get_tree_node(nodeid)
+        return target_node.name
+    #--------------------------------------
+    def get_avail_actions(self, nodeid):
+        target = self.tree.search_nodes(_nid=int(nodeid))[0]
+        action_list = []
+        for aindex, aname, aorder, show_fn, run_fn, html_generator in self.tree.actions:
+            if show_fn(target):
+                action_list.append([aindex, aname, aorder, html_generator])
+        action_list.sort(key=lambda x: x[2])
+        return action_list
+
+    def run_action(self, aindex, nodeid, astatus,a_data=-1):
+        if aindex is None:
+           return #no_action
+        
+        target = self.tree.search_nodes(_nid=int(nodeid))[0]
+        run_fn = self.tree.actions.actions[aindex][3]
+        #print "run action called in tree handler"
+        return run_fn(self.tree, target, astatus, a_data)
+    #--------------------------------------------
+    def run_tree_action(self, color_code,line_width):
+        #print "run_tree_action called..."
+        return self.treeconfig_obj.run_action_linecolorwidth(self.tree, color_code, line_width)
+
+    def run_tree_customize(self, branch, internal, ladderize):
+        #print "run_tree_customize called..."
+        self.treeconfig_obj.set_custom_options(branch, internal)
+        if ladderize:
+           self.treeconfig_obj.run_action_ladderize(self.tree)
+
+    #---------------------------------------------
+   
+class NodeActions(object):
+    def __str__(self):
+        text = []
+        for aindex, aname, aorder, show_fn, run_fn, html_generator in self:
+            text.append("%s: %s, %s, %s, %s" %(aindex, aname, show_fn, run_fn, html_generator))
+        return '\n'.join(text)
+
+    def __iter__(self):
+        for aindex, (aname, aorder, show_fn, run_fn, html_generator) in self.actions.items():
+            yield (aindex, aname, aorder, show_fn, run_fn, html_generator)
+
+    def __init__(self):
+        self.actions = {}
+
+    def clear_default_actions(self):
+        self.actions = {}
+
+    def add_action(self, action_strid, action_name, display_order, show_fn, run_fn, html_generator):
+        #aindex = "act_"+id_generator()
+        aindex = re.sub(r"\s+", '_', action_strid)
+        self.actions[aindex] = [action_name, display_order, show_fn, run_fn, html_generator]
+
---
+++ Treeviewer_Data_Service/data_handler.py
@@ -0,0 +1,456 @@
+import pymongo 
+import json
+import types
+import urllib2
+import requests
+import re
+import time
+import os
+import operator
+
+from os.path import dirname, abspath
+from sys import argv
+from ete3 import Tree
+from ete3.parser.newick import NewickError
+
+#from bson import json_util
+#from bson.json_util import dumps
+
+dbName = "EOL_data"
+dataCollectionName ="species_info"
+counterCollectionName = "speciesCounter"
+image_root_loc = ""
+#===================================================
+#connection to Mongo DB
+def connect_mongodb(host='localhost', port=27017):
+ 	try:
+ 		conn=pymongo.MongoClient(host, port)
+ 		#print "Connected to MongoDB successfully!!!"
+ 	except pymongo.errors.ConnectionFailure, e:
+ 		print "Could not connect to MongoDB: %s" % e 
+
+ 	return conn
+
+#------------------------------------------------------------
+#create list of nonexistent species in mongodb 
+def find_nonexistent_species(sp_list, data_collection):
+ 	nonexistent_species = []
+ 	for sp in sp_list:	
+ 		sp_info = find_species_info(sp, data_collection)	
+ 		if sp_info is None: #does not have any data 		
+ 			nonexistent_species.append({"species_name": sp, "eol_id": ""}) 
+ 		else: #have link data, need to get image data or already have image data
+ 			if (type(sp_info['images']) == types.DictType) and sp_info['eol_id'] != -1: #need to get image data
+ 			   nonexistent_species.append({"species_name": sp, "eol_id": sp_info['eol_id'], "species_id":sp_info['species_id']})
+ 	
+ 	return nonexistent_species
+
+#---------------------------------------------------------------
+#insert species info to collection 
+def insert_species_info(species_info, data_collection): 	
+ 	if type(species_info) == types.DictType:
+ 		result = data_collection.insert_one(species_info)
+ 		ids = result.inserted_id
+ 	elif type(species_info) == types.ListType: 
+ 		#print species_info	
+ 		result = data_collection.insert_many(species_info)	
+  		ids = result.inserted_ids
+
+ 	#return len(ids) #number of items inserted
+
+#----------------------------------------------------------
+#update species info in collection
+def update_species_info(sp_name, sp_id, sp_image_data, data_collection):
+ 	data_collection.update({"species_id": sp_id, "species_name": sp_name},{"$set": {"images": sp_image_data}})
+ 	
+#---------------------------------------------------------
+#find species info in collection 
+def find_species_info(sp_name, data_collection):
+ 	species_info = data_collection.find_one({"species_name": sp_name})
+
+ 	return species_info
+
+#----------------------------------------------------------
+#get image and link info of nonexistent species in mongodb
+def get_species_info(sp_list, data_collection, counter_collection):
+ 	sequence = "unique_species_id"	
+ 	for sp in sp_list:
+ 		sp_name = sp['species_name']
+ 		sp_eol_id = sp['eol_id']
+
+ 		sp_info = {}
+ 		sp_info['species_name'] = sp_name
+ 		if sp_eol_id != "" and sp_eol_id != -1: #eol data id is available in database
+ 			sp_id = sp['species_id']
+ 			#get image data using image web service
+ 			sp_image_data = get_image_data(sp_name, sp_eol_id)
+ 			if sp_image_data is not None:
+ 				sp_img_info = create_species_image_info(sp_name, sp_image_data, sp_id)			
+ 				sp_info['images'] = sp_img_info
+ 			else:
+ 				sp_info['images'] = []
+ 			update_species_info(sp_name, sp_id, sp_info['images'], data_collection)
+ 		elif sp_eol_id == "": #no eol data in database
+ 			#get link data using link web service			
+ 			sp_link_data = get_link_data(sp_name)
+ 			sp_id = getNextSequence(counter_collection, sequence)
+ 			sp_info['species_id'] = sp_id
+ 			if sp_link_data is not None:
+ 				sp_info['link'] = sp_link_data['eol_link']
+ 				sp_info['eol_id'] = sp_link_data['eol_id']	
+ 				sp_image_data = get_image_data(sp_name, sp_info['eol_id'])
+ 				if sp_image_data is not None:
+ 					sp_img_info = create_species_image_info(sp_name, sp_image_data, sp_id)			
+ 					sp_info['images'] = sp_img_info
+ 				else:
+ 					sp_info['images'] = []
+ 			else:
+ 				sp_info['link'] = ""
+ 				sp_info['eol_id'] = -1
+ 				sp_info['images'] = []
+ 			insert_species_info(sp_info, data_collection)
+
+#-----------------------------------------------------
+#create image info of nonexistent species in mongodb
+def create_species_image_info(sp_name, sp_image_data, sp_id):
+ 	image_info_list = []	
+ 	img_count = 0
+ 	for sp_image in sp_image_data:
+ 		thumb_url = sp_image['eolThumbnailURL']
+ 		img_count += 1
+ 		img_format = thumb_url[thumb_url.rindex(".")+1:len(thumb_url)]
+ 		relative_path = "/images/" + re.sub(r"\s+", '_', sp_name) + "_" + str(img_count) + "." + img_format
+ 		absolute_path = image_root_loc + relative_path
+ 		if not(download_image(thumb_url, absolute_path)):
+ 			img_count -= 1
+ 			continue
+
+ 		image_info = {} 
+ 		image_info['thumb_id'] = img_count
+ 		image_info['thumb_http_url'] = thumb_url
+ 		image_info['thumb_local_path'] = relative_path
+ 		
+ 		data_ratings = float(sp_image['dataRating'])
+ 		if (data_ratings > 3.5 and data_ratings <= 4.0): 
+ 			image_info['thumb_likes'] = 3
+ 		elif (data_ratings > 3.0 and data_ratings <= 3.5):
+ 			image_info['thumb_likes'] = 2
+ 		elif (data_ratings > 2.5 and data_ratings <= 3.0):
+ 			image_info['thumb_likes'] = 1
+ 		else:
+ 			image_info['thumb_likes'] = 0
+
+ 		image_info['media_url'] = sp_image['eolMediaURL']
+ 		image_info['license'] = sp_image['license']
+ 		image_info['rights_holder'] = sp_image['rightsHolder']
+ 		image_info['vetted_status'] = sp_image['vettedStatus']
+ 		image_info['data_rating'] =	sp_image['dataRating']
+ 		image_info_list.append(image_info)		
+            
+ 	return image_info_list
+#-----------------------------------------------------
+#update image score of an existent species in mongodb
+def update_species_img_score(sp_name, thumb_img_id, data_collection):
+ 	result = data_collection.update({ "species_name": sp_name, "images.thumb_id": thumb_img_id},{'$inc': {"images.$.thumb_likes": 1} })
+ 	#result_json = json.loads(result)	
+ 	#return True if result_json['Modified'] == 1 else False
+ 	
+#---------------------------------------------------------
+#create a sequence for the species_id (in 'speciesCounter' collection)
+def	createNewSequence(collection, seq_name): 
+ 	collection.insert({'_id': seq_name, 'seq': 0}) #returns seq_name when inserts successfully
+
+#----------------------------------------------------------
+#remove a sequence for the species_id (in 'speciesCounter' collection)
+def removeSequence(collection, seq_name):
+ 	collection.remove({'_id': seq_name}) #returns {u'ok': 1, u'n': 1} when removes successfully 
+
+#-------------------------------------------------------
+#reduce a sequence with a value
+def reduceSequence(collection, seq_name):
+ 	return collection.find_and_modify(query= { '_id': seq_name },update= { '$inc': {'seq': -1}}, new=True ).get('seq')
+
+#-----------------------------------------------------------
+#get next sequence for the species_id
+def getNextSequence(collection, seq_name):   
+ 	return collection.find_and_modify(query= { '_id': seq_name },update= { '$inc': {'seq': 1}}, new=True ).get('seq')  
+
+#--------------------------------------------------------------
+def get_image_data(sp_name=None, sp_eol_id=None):
+    if sp_eol_id is None:
+       image_service_uri = "http://phylo.cs.nmsu.edu:5004/phylotastic_ws/si/eol/images"
+       image_service_payload = {'species': [sp_name]}
+       service_response = execute_webservice(image_service_uri, json.dumps(image_service_payload), {'content-type': 'application/json'})
+       img_lst = service_response['species'][0]['images']
+    else:
+       num_tries = 0
+       while num_tries < 6:
+          try:
+              image_service_uri = "http://phylo.cs.nmsu.edu:5004/phylotastic_ws/si/eol/get_image"
+              #print sp_eol_id
+              image_service_payload = {'species_id': str(sp_eol_id)}
+              service_response = execute_webservice(image_service_uri, image_service_payload)
+              if service_response is not None:
+                 img_lst = service_response['species']['images']
+              else:
+                 img_lst = []
+              break  
+          except KeyError as e:
+              print "Exception caught"
+              num_tries += 1
+              pass
+    print img_lst
+    #print "image data response %s" %service_response
+    if len(img_lst) != 0:
+       species_img_urls = img_lst
+    else:
+       species_img_urls = None
+
+    return species_img_urls
+
+#--------------------------------------------------------------
+def get_link_data(sp_name):
+    eol_service_uri = "http://phylo.cs.nmsu.edu:5004/phylotastic_ws/sl/eol/links"
+    eol_service_payload = {'species': [sp_name]}
+    
+    service_response = execute_webservice(eol_service_uri, json.dumps(eol_service_payload),{'content-type': 'application/json'})
+    if service_response['species'][0]['matched_name'] == '':
+       species_link_info = None
+    else:
+       species_link_info = {'species_name': sp_name, 'eol_id': service_response['species'][0]['eol_id'], 'eol_link': service_response['species'][0]['species_info_link']}
+       
+    return species_link_info
+
+#-----------------------------------------------------------
+def execute_webservice(service_url, service_payload, header=None):
+    if header is None:
+       response = requests.post(service_url, data=service_payload)
+    else:    
+       response = requests.post(service_url, data=service_payload, headers=header)
+
+    if response.status_code == requests.codes.ok:        
+        res_json = json.loads(response.text)
+    else:
+        res_json = None
+
+    return res_json 
+
+#------------------------------------------------
+def timeit(f):
+    def a_wrapper_accepting_arguments(*args, **kargs):
+        t1 = time.time()
+        r = f(*args, **kargs)
+        print " %0.3f secs: %s" %(time.time() - t1, f.__name__)
+        return r
+    return a_wrapper_accepting_arguments
+
+#-----------------------------------------------------
+#download the image from http url and save it on local file system
+@timeit
+def download_image(http_url, local_path):
+ 	abs_file_path = image_root_loc + local_path
+ 	#print abs_file_path    
+ 	if os.path.isfile(abs_file_path):
+ 		print "%s exists" %abs_file_path
+ 		return True 
+ 	try:	
+ 		response = urllib2.urlopen(http_url)
+ 		with open(local_path, 'w') as f: 
+ 			f.write(response.read())
+ 	except urllib2.HTTPError as e: #http error
+ 		return False #image download failed
+ 	except urllib2.URLError as e: #connection error: no route to the specified server
+ 		return False #image download failed
+ 	else:
+ 		return True #image downloaded successfully
+
+
+#-----------------------------------------------------
+@timeit
+def list_info_controller(species_list):
+ 	conn = connect_mongodb()	
+ 	db = conn[dbName]
+ 	counter_collection = db[counterCollectionName]
+ 	data_collection = db[dataCollectionName]
+ 	set_image_loc()
+ 	nonexistent_species = find_nonexistent_species(species_list, data_collection)
+ 	if len(nonexistent_species) != 0:
+ 	 	get_species_info(nonexistent_species, data_collection, counter_collection)
+
+ 	return True
+#--------------------------------------------------------
+def set_image_loc():
+ 	global image_root_loc
+ 	#get the parent directory of the directory containing the script
+ 	d = dirname(dirname(abspath(__file__)))
+ 	image_root_loc = d
+
+#----------------------------------------------------------
+def estimate_image_download(newick_str):
+ 	conn = connect_mongodb()	
+ 	db = conn[dbName]
+ 	data_collection = db[dataCollectionName]
+ 	species_list = get_leaves(newick_str)
+ 	
+ 	extra_delay = 5.0
+ 	avg_download_time = 0.50*5  #5 images by 0.40s/image
+ 	num_nonexistent_species = 0
+ 	estimated_download_time = 0.0
+	
+ 	if len(species_list) !=0:
+ 		nonexistent_species = find_nonexistent_species(species_list, data_collection)
+ 		num_nonexistent_species = len(nonexistent_species)
+  		estimated_download_time = num_nonexistent_species * avg_download_time + extra_delay   
+        print "estimate completed"      	
+ 	return {"download_time": estimated_download_time, "number_species": num_nonexistent_species}
+
+#---------------------------------------------------------
+def load_all_images(newick_str):
+ 	load_status = find_image_leaves(newick_str)
+        print "loading done"
+ 	return {"download_complete": load_status}
+
+#---------------------------------------------
+def images_exists(species):
+ 	conn = connect_mongodb()	
+ 	db = conn[dbName]
+ 	data_collection = db[dataCollectionName]
+ 	species_info = find_species_info(species, data_collection)
+ 	if species_info is None: 
+ 		return {"image_info_exists": False, "species": species}
+ 	else:
+ 		images_obj = species_info['images']
+ 		#check whether the images obj is list_type or dic_type
+ 		if type(images_obj) == types.DictType: #need to get image data and update record
+ 			return {"image_info_exists": False, "species": species}
+ 		elif type(images_obj) == types.ListType:
+ 			return {"image_info_exists": True, "species": species, "image_info": images_obj}			
+   
+#---------------------------------------------------
+def image_info_controller(species, image_id, next_image):
+ 	conn = connect_mongodb()	
+ 	db = conn[dbName]
+ 	counter_collection = db[counterCollectionName]
+ 	data_collection = db[dataCollectionName]
+ 	set_image_loc()
+ 	nonexistent_species = find_nonexistent_species([species], data_collection)
+ 	if len(nonexistent_species) != 0:
+ 		get_species_info(nonexistent_species, data_collection, counter_collection)
+ 	species_info = find_species_info(species, data_collection)
+ 	
+ 	result_info = {} 
+ 	images_obj = species_info['images']
+ 	sp_id = species_info['species_id']
+ 	sp_eol_id = species_info['eol_id']
+ 	#check whether the images obj is list_type or dic_type
+ 	if type(images_obj) == types.DictType: #need to get image data and update record
+ 		sp_image_data = get_image_data(species, sp_eol_id)
+  		sp_id = species_info['species_id']
+ 		if sp_image_data is not None:
+ 			sp_img_info = create_species_image_info(species, sp_image_data, sp_id)	
+ 		else:
+ 			sp_img_info = []
+ 		update_species_info(species, sp_id, sp_img_info, data_collection)
+ 		images_obj = sp_img_info
+
+ 	if len(images_obj) != 0 and image_id != 0 and not(next_image):		
+ 		for image in images_obj:
+ 			if image_id == image['thumb_id']:
+ 				result_info = image
+ 	elif len(images_obj) != 0 and image_id !=0 and next_image:
+ 		next_image = get_next_image(images_obj, image_id)
+ 		result_info = next_image
+ 	elif len(images_obj) != 0 and image_id == 0:
+ 		popular_image = get_popular_image(images_obj) 
+ 		result_info = popular_image
+ 	
+ 	conn.close()
+ 	return result_info
+
+#------------------------------------------------------
+def link_info_controller(species):
+ 	conn = connect_mongodb()	
+ 	db = conn[dbName]
+ 	data_collection = db[dataCollectionName]
+ 	counter_collection = db[counterCollectionName]
+ 	sequence = "unique_species_id"
+
+ 	species_info = find_species_info(species, data_collection)
+ 	sp_info = {}	
+ 	if species_info is None: #info is not in database
+ 		sp_link_data = get_link_data(species)
+ 		sp_id = getNextSequence(counter_collection, sequence)
+ 		if sp_link_data is not None:
+ 			sp_info['eol_link'] = sp_link_data['eol_link']
+ 			sp_info['eol_id'] = sp_link_data['eol_id']
+ 			sp_image = {}			  
+ 		else:
+ 			sp_info['eol_link'] = ""
+ 			sp_info['eol_id'] = -1
+ 			sp_image = []
+
+ 		species_info = {'species_id': sp_id, 'images': sp_image, 'species_name': species, 'link': sp_info['eol_link'], 'eol_id': sp_info['eol_id']}
+ 		insert_species_info(species_info, data_collection)
+ 	else: #info is in database
+ 	 	sp_info['eol_link'] = species_info['link']	
+ 	
+ 	sp_info['species'] = species
+ 	conn.close()
+
+ 	return sp_info
+
+#------------------------------------------------------------
+def get_popular_image(image_list):
+ 	images = {}
+ 	for img in image_list:
+ 		images[img['thumb_id']] = int(img['thumb_likes'])
+
+ 	sorted_images = sorted(images.items(), key=operator.itemgetter(1), reverse=True)
+ 	popular_img_id = sorted_images[0][0]
+ 	for img in image_list:
+ 		if img['thumb_id'] == popular_img_id:
+ 			return img
+
+#--------------------------------------------------------------
+def get_next_image(image_list, image_id):
+ 	for img_index, img in enumerate(image_list):
+ 		if (image_id == img['thumb_id']):
+ 			current_img_index = img_index
+
+ 	next_img_index = (current_img_index+1)%(len(image_list))
+ 	return image_list[next_img_index]
+
+#------------------------------------------------------------
+def parse_newick(newick_str):
+ 	try:
+ 		print newick_str      
+ 		tree = Tree(newick_str)
+ 		print tree
+ 	except NewickError:
+ 		try:
+ 			tree = Tree(newick_str, format=1)
+ 		except NewickError as e:
+ 			return None
+ 	
+ 	return tree 
+
+#--------------------------------------------------------------
+def get_leaves(newick_str):
+ 	species_list = []
+ 	treeobj = parse_newick(newick_str)
+ 	if treeobj is not None:
+ 		species_list = [leaf.name for leaf in treeobj.iter_leaves()]
+ 	
+ 	return species_list
+
+#-------------------------------------------------------------
+def find_image_leaves(newick_str):
+ 	speciesList = get_leaves(newick_str)
+ 	if len(speciesList) != 0:
+ 	 	return list_info_controller(speciesList)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#if __name__ == "__main__":
+
+# 	script, newick_str = argv
+# 	find_image_leaves(newick_str)
