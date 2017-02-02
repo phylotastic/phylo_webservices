ea837afecb1fcd344b1c5ba5733566d760522b65
Abu Saleh <abusalehmdtayeen@gmail.com>
2017-02-01 19:55:23 -0700

    Added metadata to name resolver services

--- Phylotastic_UI_WebServices/support/resolve_names_service.py
+++ Phylotastic_UI_WebServices/support/resolve_names_service.py
@@ -5,10 +5,12 @@ import requests
 import re
 import ast
 import urllib
+import datetime
 
 
 api_url = "http://resolver.globalnames.org/name_resolvers.json?"
 headers = {'content-type': 'application/json'}
+base_url = "http://phylo.cs.nmsu.edu:5004/phylotastic_ws/tnrs/"
 
 #~~~~~~~~~~~~~~~~~~~~ (GlobalNamesResolver-TNRS)~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #resolve scientific names
@@ -26,23 +28,24 @@ def resolve_sn_gnr(scNames, post):
     if response.status_code == requests.codes.ok:    
         data_json = json.loads(response.text)
         rsnames_list = data_json['data']
+        parameters_list = data_json['parameters']   
         for element in rsnames_list:
             rsname = element['results'][0]['canonical_form']       
- 	    namesList = {}
- 	    namesList['matched_name'] =  rsname
+            namesList = {}
+            namesList['matched_name'] =  rsname
             namesList['search_string'] = element['supplied_name_string']
- 	    namesList['match_type'] = 'Exact' if element['results'][0]['match_type'] == 1 else 'Fuzzy'
- 	    namesList['synonyms'] = []
- 	    namesList['taxon_id'] = element['results'][0]['taxon_id']	
+            namesList['match_type'] = 'Exact' if element['results'][0]['match_type'] == 1 else 'Fuzzy'
+            namesList['synonyms'] = []
+            namesList['taxon_id'] = element['results'][0]['taxon_id']	
             namesList['resolver_name'] = 'GNR'
- 	    resolvedNamesList.append(namesList)
+            resolvedNamesList.append(namesList)
  	    status_code = 200
     else:
         status_code = 500
-    if post: 	    
-        return {'resolvedNames': resolvedNamesList, 'status_code': status_code}
-    else:
-        return json.dumps({'resolvedNames': resolvedNamesList, 'status_code': status_code}) 
+
+    service_documentation = "https://github.com/phylotastic/phylo_services_docs/blob/master/ServiceDescription/PhyloServicesDescription.md#web-service-4"
+
+    return {'resolvedNames': resolvedNamesList, 'parameters': parameters_list, "service_url_doc": service_documentation, 'status_code': status_code}
         
 #----------------------------------------------    
 
@@ -86,7 +89,7 @@ def resolve_sn_ot(scNames, do_fuzzy_match, multi_match):
         status_code = 200
     else:
         status_code = 500
-    
+
     return {'resolvedNames': resolvedNamesList, 'status_code': status_code}
  
 #-------------------------------------------
@@ -126,13 +129,15 @@ def resolve_names_OT(inputNamesList, do_fuzzy_match=True, multi_match=False, pos
     list_size = 250
     final_result = []
 
+    start_time = time.time()
+
     if len(inputNamesList) > list_size:
     	sublists = create_sublists(inputNamesList, list_size)
     	for sublst in sublists:
     		resolvedResult = resolve_sn_ot(sublst, do_fuzzy_match, multi_match)
     		resolvedNameslst = resolvedResult['resolvedNames']
     		final_result.extend(resolvedNameslst)
-    	status_code = resolvedResult['status_code']
+    	status_code = resolvedResult['status_code']  
     else:
     	resolvedResult = resolve_sn_ot(inputNamesList, do_fuzzy_match, multi_match)
     	final_result = resolvedResult['resolvedNames']
@@ -141,20 +146,41 @@ def resolve_names_OT(inputNamesList, do_fuzzy_match=True, multi_match=False, pos
     if len(final_result) <= 0 and status_code != 500:
     	status_code = 204
 
+    end_time = time.time()
+    result_len = len(final_result)
+  
+    service_documentation = "https://github.com/phylotastic/phylo_services_docs/blob/master/ServiceDescription/PhyloServicesDescription.md#web-service-3"
+    execution_time = "{:4.2f}".format(end_time-start_time)
+    #service result creation time
+    creation_time = datetime.datetime.now().isoformat()
+
     if post: 	    
-     	return {'resolvedNames': final_result, 'status_code': status_code}
+     	return {'resolvedNames': final_result, 'creation_time': creation_time, 'execution_time': execution_time, 'total_names': result_len, 'status_code': status_code, "service_url_doc": service_documentation}
     else: 
-        return json.dumps({'resolvedNames': final_result, 'status_code': status_code}) 
+        return json.dumps({'resolvedNames': final_result, 'creation_time': creation_time, 'execution_time': execution_time, 'total_names': result_len, 'status_code': status_code, "service_url_doc": service_documentation}) 
 
 #-----------------------------------------------------------
 def resolve_names_GNR(inputNamesList, post=False): 
     
+    start_time = time.time()
     api_friendly_list = make_api_friendly_list(inputNamesList)	
     final_result = resolve_sn_gnr(api_friendly_list, post)    
-    
-    return final_result
+    end_time = time.time()
+    execution_time = end_time-start_time
+
+    #service result creation time
+    creation_time = datetime.datetime.now().isoformat()
+    final_result['creation_time'] = creation_time
+    final_result['execution_time'] = "{:4.2f}".format(execution_time)
+    final_result['total_names'] = len(final_result['resolvedNames']) 
+
+    if post: 	    
+        return final_result
+    else:
+        return json.dumps(final_result)  
 
 #------------------------------------------------------
+'''
 def write_result(final_result):
     result_file = open('tnrs_response2.txt', 'w')
     result_file.write(str(final_result))
@@ -165,18 +191,17 @@ def read_input():
  	with open('tnrs_input.txt', 'r') as f:
  		read_data = f.read()
  	return read_data
+'''
 #-------------------------------------------------
 #if __name__ == '__main__':
 
-    #inputList = ["Astar"]
+    #inputList = ["Formica polyctena", "Tetramorium caespitum","Carebara diversa", "Formicinae"]
     #inputList = ["Setophaga plambea", "Setophaga angilae", "Setophaga magnolia", "Setophaga strieta", "Setophaga virens"]
     
     #result = resolve_names_GNR(inputList)    
     #print result
-    #st_time = time.time()
+    
     #result = resolve_names_OT(inputList, True, True, True)
-    #en_time = time.time()
     #print result
-    #print en_time-st_time
     
        
