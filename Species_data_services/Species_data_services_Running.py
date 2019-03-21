8ebaa35906ca42fe7c4771d6f1fc37866662c7dd
Abu Saleh <abusalehmdtayeen@gmail.com>
2019-03-21 07:45:48 -0600

    EBI common name service added

--- Species_data_services/Species_data_services_Running.py
+++ Species_data_services/Species_data_services_Running.py
@@ -16,6 +16,7 @@ from support import conservation_service_ECOS
 from support import common_name_species_service_NCBI
 from support import common_name_species_service_ITIS
 from support import common_name_species_service_TROPICOS
+from support import common_name_species_service_EBI
 
 from support import scientific_to_common_name_NCBI
 from support import scientific_to_common_name_EOL
@@ -601,6 +602,111 @@ class Common_Name_Species_Service_TROPICOS_API(object):
     get_scientific_names.exposed = True
     scientific_names.exposed = True
 
+#==================================Common Name Service: EBI=======================================
+class Common_Name_Species_Service_EBI_API(object):
+    def index(self):
+        return "Common_Name_Species_Service_EBI_API : Get scientific names from common names using EBI source";
+
+    #---------------------------------------------
+    @cherrypy.tools.json_out()
+    def get_scientific_names(self, **request_data):
+        try:
+            http_method = cherrypy.request.method
+            if http_method not in ['GET']:
+               return return_response_error(405,"Error: HTTP Methods other than GET are not allowed","JSON")
+
+            name_lst = str(request_data['commonnames']).strip()
+            name_lst = name_lst.split('|')
+
+            if len(name_lst) == 1 and '' in name_lst: 
+               raise CustomException("'commonnames' parameter must have a valid value")
+            
+            multi_match = False
+            if request_data is not None and 'multiple_match' in request_data:
+               multi_match = str(request_data['multiple_match']).strip()
+               if type(multi_match) != types.BooleanType:
+                  multi_match = str2bool(multi_match)
+            
+            if len(name_lst) > 20: 
+               return return_response_error(403,"Error: Currently more than 20 names is not supported","JSON")
+
+        except KeyError, e:
+            return return_response_error(400,"Error: Missing parameter %s"%(str(e)),"JSON")
+        except CustomException, e:
+            return return_response_error(400,"Error: %s"%(str(e)),"JSON")   
+        except Exception, e:
+            return return_response_error(500,"Error: %s"%(str(e)), "JSON")
+        
+        try: 
+           service_result = common_name_species_service_EBI.get_scientific_names(name_lst, not multi_match)
+           #-------------------------------------------
+           header = cherrypy.request.headers
+           log = {'client_ip': cherrypy.request.remote.ip, 'date': datetime.datetime.now(), 'request_base': cherrypy.request.base, 'request_script': cherrypy.request.script_name, 'request_path': cherrypy.request.path_info, 'method': cherrypy.request.method, 'params': cherrypy.request.params, 'user_agent': header['User-Agent'], 'response_status': service_result['status_code']}
+           insert_log(log)
+           #---------------------------------------------
+           if service_result['status_code'] == 200:
+               return service_result
+           else:
+               return return_response_error(service_result['status_code'], service_result['message'], "JSON")
+
+        except Exception, e:
+            cherrypy.log("====CommonName_EBI_GET_Error====", traceback=True)
+            return return_response_error(500,"Error: %s"%(str(e)), "JSON")
+
+    
+ 	#-----------------------------------------------	
+    @cherrypy.tools.json_out()
+    @cherrypy.tools.json_in()
+    def scientific_names(self,**request_data):
+        try:
+            http_method = cherrypy.request.method
+            if http_method not in ['POST']:
+               return return_response_error(405,"Error: HTTP Methods other than POST are not allowed","JSON")
+
+            input_json = cherrypy.request.json
+            namelist = input_json["commonnames"]
+            if type(namelist) != types.ListType:
+               return return_response_error(400,"Error: 'commonnames' parameter must be of list type","JSON")
+
+            multi_match = False
+            if 'multiple_match' in input_json:
+               multi_match = input_json['multiple_match']
+               if type(multi_match) != types.BooleanType:
+                  multi_match = str2bool(multi_match)
+    
+            if len(namelist) > 20: 
+               return return_response_error(403,"Error: Currently more than 20 names is not supported","JSON")
+ 
+        except KeyError, e:
+            return return_response_error(400,"Error: Missing parameter %s"%(str(e)),"JSON")
+        except CustomException, e:
+            return return_response_error(400,"Error: %s"%(str(e)),"JSON")   
+        except Exception, e:
+            return return_response_error(500,"Error: %s"%(str(e)), "JSON")
+        
+        try: 
+            service_result = common_name_species_service_EBI.get_scientific_names(namelist, not multi_match) 
+            #--------------------------------------------
+            header = cherrypy.request.headers
+            log = {'client_ip': cherrypy.request.remote.ip, 'date': datetime.datetime.now(), 'request_base': cherrypy.request.base, 'request_script': cherrypy.request.script_name, 'request_path': cherrypy.request.path_info, 'method': cherrypy.request.method, 'params': {'commonnames': namelist}, 'user_agent': header['User-Agent'], 'response_status': service_result['status_code']}
+            insert_log(log)
+ 
+            if service_result['status_code'] == 200:
+               return service_result
+            else:
+               return return_response_error(service_result['status_code'], service_result['message'], "JSON")
+
+        except Exception, e:
+            cherrypy.log("====CommonName_EBI_POST_Error====", traceback=True)
+            return return_response_error(500,"Error: %s"%(str(e)), "JSON")
+
+    #-----------------------------------------------------
+    #Public /index
+    index.exposed = True
+    get_scientific_names.exposed = True
+    scientific_names.exposed = True
+
+
 #===================================Scientific Name Service: NCBI====================================
 class Scientific_Name_Service_NCBI_API(object):
     def index(self):
@@ -902,7 +1008,8 @@ if __name__ == '__main__':
     cherrypy.tree.mount(Conservation_ECOS_Service_API(), '/%s/%s/%s' %(str(WS_NAME),str(WS_GROUP1), "ecos"), conf_CORS)
     cherrypy.tree.mount(Common_Name_Species_Service_NCBI_API(), '/%s/%s/%s' %(str(WS_NAME),str(WS_GROUP2),"ncbi"),conf_CORS )
     cherrypy.tree.mount(Common_Name_Species_Service_ITIS_API(), '/%s/%s/%s' %(str(WS_NAME),str(WS_GROUP2),"itis"),conf_CORS )
-    cherrypy.tree.mount(Common_Name_Species_Service_TROPICOS_API(), '/%s/%s/%s' %(str(WS_NAME),str(WS_GROUP2),"tpcs"),conf_CORS )	
+    cherrypy.tree.mount(Common_Name_Species_Service_TROPICOS_API(), '/%s/%s/%s' %(str(WS_NAME),str(WS_GROUP2),"tpcs"),conf_CORS )
+	cherrypy.tree.mount(Common_Name_Species_Service_EBI_API(), '/%s/%s/%s' %(str(WS_NAME),str(WS_GROUP2),"ebi"),conf_CORS )
 
     cherrypy.tree.mount(Scientific_Name_Service_NCBI_API(), '/%s/%s/%s' %(str(WS_NAME),str(WS_GROUP3),"ncbi"),conf_CORS )
     cherrypy.tree.mount(Scientific_Name_Service_EOL_API(), '/%s/%s/%s' %(str(WS_NAME),str(WS_GROUP3),"eol"),conf_CORS )	   	
---
+++ Species_data_services/support/common_name_species_service_EBI.py
@@ -0,0 +1,92 @@
+#service description: get scientific names for input common names using European Bioinformatics Institute Taxonomy database 
+#service version: 0.1
+#source: https://www.ebi.ac.uk/ena/browse/taxonomy-service
+import json
+import requests
+import time
+import datetime 
+import urllib
+
+#----------------------------------------------
+base_url = "http://www.ebi.ac.uk/ena/data/taxonomy/v1/taxon/"
+#-----------------------------------------------------
+
+def search_name(commonName, best_match):
+
+	api_url = base_url + "any-name/" if best_match else base_url + "suggest-for-search/"	
+	rest_api = api_url + commonName
+
+	response = {}    
+	response['status_code'] = 200
+ 	response['message'] = "Success"
+	
+	api_response = requests.get(rest_api) 
+	
+	match_name_info = {'searched_name': commonName}
+	match_name_list = []
+
+	if api_response.status_code == requests.codes.ok:
+		#print api_response.text	 	
+		match_name_list = extract_sc_names_info(api_response.text, not best_match) #best match true = don't want multiple results	
+	else:
+		response['status_code'] = 500
+		response['message'] = "Error: Retrieving data from EBI API"
+
+	match_name_info['matched_names'] = match_name_list
+	response['result'] = match_name_info 
+
+	return response
+
+#----------------------------------------------
+def extract_sc_names_info(response_result, multiple_match=False):
+	matched_names = []
+	json_result = json.loads(response_result)
+	for indx, result in enumerate(json_result):
+		match_name_obj = {}
+		match_name_obj['scientific_name'] = result['scientificName']
+		match_name_obj['common_name'] = result['commonName']  
+		match_name_obj['identifier'] = result['taxId']
+		matched_names.append(match_name_obj)
+		if not(multiple_match):
+			break
+		if indx == 20:
+			break
+
+	return matched_names
+
+#---------------------------------------------------
+def get_scientific_names(inputNameList, best_match=True):	
+ 	start_time = time.time()
+	
+	final_result = {'status_code': 200, 'message': "Success" }
+
+	results = []
+	for inputName in inputNameList:
+		match_result = search_name(inputName, best_match)
+		if match_result['status_code'] == 200:
+			results.append(match_result['result'])
+
+	final_result['result'] = results
+
+	end_time = time.time()
+ 	execution_time = end_time-start_time    
+    
+	#service result creation time
+ 	creation_time = datetime.datetime.now().isoformat()
+
+	final_result['metadata'] = {'creation_time': creation_time, 'execution_time': "{:4.2f}".format(execution_time), 'source_urls': ["https://www.ebi.ac.uk"] }   	 
+ 	
+ 	return final_result
+#--------------------------------------------------
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#if __name__ == '__main__':
+
+	#commonNameList = ["christmas fern", "castor bean", "indian sandalwood", "annual blue grass"]
+	#commonNameList = ["corn", "rice", "wheat", "potato", "onion", "garlic", "cucumber", "tomato", "lettuce", "pea"]
+	#commonNameList = ["christmas fern", "castor bean", "indian sandalwood", "annual blue grass"]
+	#commonNameList = ["cow", "cat", "horse"]
+	
+	#print get_scientific_names(commonNameList)
+ 	
