8217632fa56acd5f2ef7786729956f9388553284
Abu Saleh <abusalehmdtayeen@gmail.com>
2019-09-23 21:33:34 -0600

    fixed NCBI API issue

--- Phylotastic_UI_WebServices_New/support/taxon_genome_species_service_NCBI.py
+++ Phylotastic_UI_WebServices_New/support/taxon_genome_species_service_NCBI.py
@@ -5,10 +5,11 @@ import requests
 import time
 import datetime 
 import urllib
+import sys
 
 #----------------------------------------------
 base_url = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/"
-headers = {'content-type': 'application/json'}
+headers = {"Content-Type": "application/x-www-form-urlencoded"}
 
 #----------------------------------------------
 #get genome ids available for a named taxonomic group
@@ -48,7 +49,7 @@ def find_genome_ids(taxonName):
  		genome_response['message'] = "Error: exception in find_genome_ids() method"
 
 	genome_response['genome_ids'] = gid_list
- 	 	
+ 	#print len(gid_list) 	 	
  	return genome_response 	
 
 #--------------------------------------------   
@@ -65,20 +66,20 @@ def find_species_ids(genomeIds):
  		'tool': "phylotastic-services",
  		'email': "tayeen@nmsu.edu"
     }
+ 	sid_list = []
+ 	taxonomy_response = {}    
  	try:	
  		encoded_payload = urllib.urlencode(payload)
  		response = requests.get(api_url, params=encoded_payload, headers=headers) 
  		#response = requests.post(api_url, data=payload, headers={"Content-Type": "application/x-www-form-urlencoded"})
  		
- 		sid_list = []
- 		taxonomy_response = {}    
  		taxonomy_response['status_code'] = 200
  		taxonomy_response['message'] = "Success"
 
- 	
  		if response.status_code == requests.codes.ok:    
  			data_json = json.loads(response.text)
  			sid_list = data_json['linksets'][0]['linksetdbs'][0]['links'] 
+ 			#print sid_list
  		else:
  			taxonomy_response['status_code'] = response.status_code
  			taxonomy_response['message'] = "Error: Response error from NCBI elink.fcgi API"
@@ -97,7 +98,7 @@ def get_species_names(speciesIds):
  	api_func = "esummary.fcgi"	
  	api_url = base_url + api_func    
  	payload = {
- 		'retmax': 5000,
+ 		'retmax': 500,
  		'retmode': 'json',
  		'db': 'taxonomy',
  		'id': speciesIds,
@@ -118,7 +119,9 @@ def get_species_names(speciesIds):
  			data_json = json.loads(response.text)
  			uid_list = data_json['result']['uids']
  			for uid in uid_list:
- 				sname_list.append(data_json['result'][str(uid)]['scientificname']) 
+ 				if 'scientificname' in data_json['result'][str(uid)]:
+ 					if 'rank' in data_json['result'][str(uid)] and data_json['result'][str(uid)]['rank'] == "species": 
+ 						sname_list.append(data_json['result'][str(uid)]['scientificname'])
  		else:
  			taxonomy_response['status_code'] = response.status_code
  			taxonomy_response['message'] = "Error: Response error from NCBI esummary.fcgi API"
@@ -128,7 +131,7 @@ def get_species_names(speciesIds):
  		taxonomy_response['message'] = "Error: exception in get_species_name() method"
 		
  	taxonomy_response['species'] = sname_list
-
+ 	#print sname_list
  	return taxonomy_response
  	
 #----------------------------------------------
@@ -145,11 +148,15 @@ def form_cs_ids(id_list):
  			
  	return str_ids
  	
+#-----------------------------------------
+def create_sublists(lst, size=500):
+	return [lst[i:i+size] for i in xrange(0, len(lst), size)]
+
 #---------------------------------------------------
 def get_genome_species(inputTaxon):	
  	start_time = time.time()
- 	#service_url = "https://phylo.cs.nmsu.edu/phylotastic_ws/ts/ncbi/genome_species?taxon=" + inputTaxon
- 	#service_documentation = "https://github.com/phylotastic/phylo_services_docs/blob/master/ServiceDescription/PhyloServicesDescription.md#web-service-9"
+ 	list_size = 300
+ 	final_species_list = []
 
  	final_result = {'status_code': 200, 'message': "Success"}	
  	g_response = find_genome_ids(inputTaxon)
@@ -157,15 +164,40 @@ def get_genome_species(inputTaxon):
  	if g_response['status_code'] != 200:	 	
  		final_result = g_response
  	else:
- 		if len(g_response['genome_ids']) != 0:
- 			str_gids = form_cs_ids(g_response['genome_ids'])
- 			s_response = find_species_ids(str_gids)
- 			
- 			if s_response['status_code'] != 200:
- 				final_result = s_response
+ 		g_id_list = g_response['genome_ids']
+ 		#print g_id_list
+ 		if len(g_id_list) != 0:
+ 			sp_list = []
+ 			if len(g_id_list) > list_size:
+ 				g_sub_lists = create_sublists(g_id_list, list_size)
+ 				for gsb_lst in g_sub_lists:
+ 					str_gids = form_cs_ids(gsb_lst)
+ 					s_response = find_species_ids(str_gids)
+ 					if s_response['status_code'] != 200:
+ 						final_result = s_response
+ 						break
+ 					sp_list.extend(s_response['species_ids'])
+ 					time.sleep(.500) #wait 500 ms to avoid choking the NCBI API and getting blocked 
  			else:
- 				str_sids = form_cs_ids(s_response['species_ids'])
- 				final_result = get_species_names(str_sids)
+ 				str_gids = form_cs_ids(g_id_list)
+ 				sp_list = find_species_ids(str_gids)['species_ids'] 						
+ 			#print sp_list
+ 			
+ 			#to handle large number of species
+ 			if len(sp_list) > list_size:
+ 				sublists = create_sublists(sp_list, list_size)
+ 				#print "Number of sublists: %d"%len(sublists)
+ 				for sublst in sublists:
+ 					str_sids = form_cs_ids(sublst)
+ 					final_species_list.extend(get_species_names(str_sids)['species'])
+ 					time.sleep(.500) #wait 500 ms to avoid choking the NCBI API and getting blocked
+ 					#print len(final_species_list)
+ 						
+ 			else:
+ 				str_sids = form_cs_ids(sp_list)
+ 				final_species_list = get_species_names(str_sids)['species']
+
+ 			final_result['species'] = final_species_list
  		else:
  			final_result['message'] = g_response['message']
  			final_result['species'] = g_response['genome_ids']
@@ -176,7 +208,7 @@ def get_genome_species(inputTaxon):
  	creation_time = datetime.datetime.now().isoformat()
  	
  	meta_data = {'creation_time': creation_time, 'execution_time': float("{:4.2f}".format(execution_time)), 'source_urls': ["https://www.ncbi.nlm.nih.gov/taxonomy", "https://www.ncbi.nlm.nih.gov/genome"]} 
-   #, 'service_documentation': service_documentation}	 #'service_url': service_url
+  
  	final_result['meta_data'] = meta_data
  	
  	if 'status_code' in final_result and final_result['status_code'] == 200: 
@@ -190,6 +222,7 @@ def get_genome_species(inputTaxon):
 #--------------------------------------------------
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#https://ncbiinsights.ncbi.nlm.nih.gov/2017/11/02/new-api-keys-for-the-e-utilities/
 
 #if __name__ == '__main__':
 
@@ -197,8 +230,11 @@ def get_genome_species(inputTaxon):
  	#inputTaxon = 'Rodentia'
 	#inputTaxon = 'Canidae' #family
  	#inputTaxon = 'Primates' #family
- 	
- 	#start_time = time.time()    
+ 	#inputTaxon= "Enterobacteriaceae"
+ 	#inputTaxon="Eukaryota"
+ 	#inputTaxon = "Animalia"
+ 	#start_time = time.time()
+ 	#print find_genome_ids(inputTaxon)        
  	#print get_genome_species(inputTaxon)
  	#end_time = time.time()
  	
